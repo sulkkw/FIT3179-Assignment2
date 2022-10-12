bed062a48dac5580900ddf4e0672a65ce0b2828d
Ko Ko Win <88918335+sulkkw@users.noreply.github.com>
2022-10-12 16:43:18 +0800

    Add files via upload

---
+++ js/vega_vis.js
@@ -0,0 +1,15 @@
+var vg_1 = "chloropeth_map.vg.json";
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "waffle_chart.vg.json";
+vegaEmbed("#waffle", vg_2, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "stack_barchart.vg.json";
+vegaEmbed("#stack", vg_3, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "linechart.vg.json";
+vegaEmbed("#line", vg_4, {"actions": false}).then(function(result) {
+}).catch(console.error);
